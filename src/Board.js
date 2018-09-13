a2e069bc71dbeb35acb673f0b9bde8576e3a9153
Vishal <vishal.b1989@gmail.com>
2018-09-13 03:02:53 -0400

    added board

---
+++ src/Board.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Note from './Note';
+
+class Board extends Component {
+    render() {
+        return (
+            <div className="board">
+                <Note/>
+            </div>
+        );
+    }
+}
+
+export default Board;
