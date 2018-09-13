0c67cfa479425ccaaa751cceab4e46b1e33bb4fa
Vishal <vishal.b1989@gmail.com>
2018-09-13 02:33:57 -0400

    binding events

--- src/Note.js
+++ src/Note.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
+import {FaPencilAlt} from 'react-icons/fa';
+import {FaTrashAlt} from  'react-icons/fa';
 
 class Note extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: false;
+        };
+        this.edit = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
+    }
+
+    edit(){
+        alert("Editing Node");
+    }
+    remove(){
+        alert('Removing Node');
+    }
     render(){
         return (
           <div className="note">
               <p>Learn React</p>
               <span>
-                  <button>Edit</button>
-                  <button>Remove</button>
+                  <button onClick={this.edit} id='edit'> <FaPencilAlt /></button>
+                  <button onClick={this.remove} id='remove'> <FaTrashAlt /></button>
               </span>
           </div>
         );
